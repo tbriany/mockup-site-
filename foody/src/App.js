59333c0db36a7095240fa8f69f6ab7b07cad3150
tbriany <brianytaveras@pursuit.org>
2020-04-02 21:38:12 -0400

    displays all components on app js

--- foody/src/App.js
+++ foody/src/App.js
@@ -1,26 +1,98 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import NavBar from './Components/NavBar';
+import Home from './Components/Home';
+import About from './Components/About';
+import Specialties from './Components/Specialties';
+import Menu from './Components/Menu';
+import Services from './Components/Services';
+import Footer from './Components/Footer';
+
+
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+        date: '',
+        time: '',
+        ppl: '',
+        firstname: '',
+        lastname: '',
+        submitted: ''
+    }
+  }
+
+  handleDateChange = (event) => {
+    let date = event.target.value 
+    console.log(date)
+    this.setState ({
+      date: date
+    })
+  }
+
+  handleTimeChange = (event) => {
+    let time = event.target.value 
+    console.log(time)
+    this.setState ({
+      time: time
+    })
+  }
+
+  handlePplChange = (event) => {
+    let ppl = event.target.value 
+    console.log(ppl)
+    this.setState ({
+      ppl: ppl
+    })
+  }
+
+  handlefirstName = (event) => {
+    let name = event.target.value
+    this.setState ({
+      firstname: name
+    })
+  }
+
+  handleLastName = (event) => {
+    let name = event.target.value
+    this.setState ({
+      lastname: name
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    const {date, time, ppl, firstname, lastname} = this.state 
+    this.setState ({
+      submitted: `Thank you ${firstname} ${lastname}! Your reservation was booked for ${date} at ${time} for ${ppl} people. See you then!`
+    })
+  }
+
+  render () {
+    console.log(this.state)
+    const {handleDateChange, handleTimeChange, handlePplChange, handlefirstName, handleLastName, handleSubmit} = this
+    const {submitted} = this.state
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar/>
+      <Home/>
+      <About/>
+      <Specialties/>
+      <Menu/>
+      <Services
+      handleDateChange = {handleDateChange}
+      handleTimeChange = {handleTimeChange}
+      handlePplChange = {handlePplChange}
+      handlefirstName = {handlefirstName}
+      handleLastName = {handleLastName}
+      handleSubmit = {handleSubmit}
+      submitted = {submitted}
+      />
+      <Footer/>
     </div>
   );
+  }
 }
 
 export default App;
